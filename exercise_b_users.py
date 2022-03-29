4a7e5e4f7886ce586f719f866c32e2f27ceb27f5
mattnorris1990 <mattnorris1990@gmail.com>
2022-03-29 16:19:34 +0100

    exb answered 2nd question

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -59,6 +59,9 @@ users = {
 print(users["Jonathan"]["twitter"])
 
 # 2. Get Erik's hometown
+
+print(users["Erik"]["home_town"])
+
 # 3. Get the list of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
