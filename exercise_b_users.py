2a874bacaf967fe806bd4805fc698d6d505f5412
mattnorris1990 <mattnorris1990@gmail.com>
2022-03-29 16:50:50 +0100

    exb answered 7th question

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -91,6 +91,10 @@ print(even_numbers)
 
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+users["Erik"]["lottery_numbers"].append(7)
+print(users["Erik"]["lottery_numbers"])
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
